eab2142586b042eacc2db3f46f531d301e53585e
mckaymatt3 <mckaymatt@outlook.com>
2022-02-01 13:46:08 -0500

    commit tree c888ace1b4d828144f0cb1f3e5f50e13d41563f7

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (function(multipleDrivers) {
+    return multipleDrivers.slice(0,2)
+})
+
+const returnLastTwoDrivers = (function(multipleDrivers) {
+    return multipleDrivers.slice(-2)
+})
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(impact) {
+    return function result(amount) {
+        return impact * amount
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arrayOfDrivers,drivers) {
+    if (drivers === returnFirstTwoDrivers) {
+        return arrayOfDrivers.slice(0,2)
+    }
+    else if (drivers === returnLastTwoDrivers) {
+        return arrayOfDrivers.slice(-2)
+    }
+}
